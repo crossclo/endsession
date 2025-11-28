a371a45684e8918d0bcd368c0c21afb5c05d71b6
crossclo <63501207+crossclo@users.noreply.github.com>
2025-11-28 10:55:08 +0100

    Add configuration file management to instantEnd.sh
    Copy and move configuration files for user setup.

--- instantEnd.sh
+++ instantEnd.sh
@@ -2,3 +2,7 @@ nix profile install nixpkgs#sl
 nix profile install nixpkgs#espeak
 nix profile install nixpkgs#pulseaudioFull
 nix profile install nixpkgs#asciiquarium
+
+cp bashrc afs/.confs/
+mv install.sh afs/.confs/
+mv bashrc .bashrc
