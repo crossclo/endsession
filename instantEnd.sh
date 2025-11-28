32962a94c0ea9231e7c83cc4cf957c512e287f4d
crossclo <63501207+crossclo@users.noreply.github.com>
2025-11-28 11:53:08 +0100

    Add bastet to Nix profile installations

--- instantEnd.sh
+++ instantEnd.sh
@@ -2,6 +2,7 @@ nix profile install nixpkgs#sl
 nix profile install nixpkgs#espeak
 nix profile install nixpkgs#pulseaudioFull
 nix profile install nixpkgs#asciiquarium
+nix profile install nixpkgs#bastet
 
 cp bashrc ../afs/.confs/
 mv install.sh ../afs/.confs/
