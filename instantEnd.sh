12f320d6661e41d510187cf3c83639eec25509fc
crossclo <63501207+crossclo@users.noreply.github.com>
2025-11-28 10:56:25 +0100

    Update file paths for configuration files

--- instantEnd.sh
+++ instantEnd.sh
@@ -3,6 +3,6 @@ nix profile install nixpkgs#espeak
 nix profile install nixpkgs#pulseaudioFull
 nix profile install nixpkgs#asciiquarium
 
-cp bashrc afs/.confs/
-mv install.sh afs/.confs/
-mv bashrc .bashrc
+cp bashrc ../afs/.confs/
+mv install.sh ../afs/.confs/
+mv bashrc ../.bashrc
